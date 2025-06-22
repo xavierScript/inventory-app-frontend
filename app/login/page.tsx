189559bcc02ece0b23c6987a7f1c296cf6a91258
xavierscript <david111francisco@gmail.com>
2025-06-22 20:24:11 +0100

    fixed login error

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -14,12 +14,21 @@ export default function LoginPage() {
     e.preventDefault();
     setLoading(true);
     setError("");
+    // try {
+    //   const res = await fetch("http://localhost:5000/api/auth/login", {
+    //     method: "POST",
+    //     headers: { "Content-Type": "application/json" },
+    //     body: JSON.stringify({ username, password }),
+    //   });
     try {
-      const res = await fetch("http://localhost:5000/api/auth/login", {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({ username, password }),
-      });
+      const res = await fetch(
+        "https://inventory-app-backend-8i8b.onrender.com/api/auth/login",
+        {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify({ username, password }),
+        }
+      );
       const data = await res.json();
       if (!res.ok) {
         setError(data.message || "Login failed");
